72064263830a22f93caef7151110c1d8fc2de440
neil xavier dsouza <neil.xavier.dsouza@gmail.com>
2019-10-26 13:22:32 -0400

    setp 16 - james willet - udemy , gatling fundamentals

--- src/gatling/scala/MyFirstTest.scala
+++ src/gatling/scala/MyFirstTest.scala
@@ -4,10 +4,16 @@ import io.gatling.http.Predef._
 class MyFirstTest extends Simulation {
 
   // 1 Common HTTP Configuration
+  val httpConf = http
+    .baseURL("http://localhost:8080/app/")
+    .header("Accept", "application/json")
 
   // 2 Scenario Definition
+  val scn = scenario("My first Test")
+    .exec(http("get All games").get("videogames"))
 
   // 3 Load Scenario
+  setUp(scn.inject(atOnceUsers(1))).protocols(httpConf)
 
 
 }
