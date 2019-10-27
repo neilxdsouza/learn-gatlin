0b33941ba997298943d634805ebb218ce86a170c
neil xavier dsouza <neil.xavier.dsouza@gmail.com>
2019-10-27 13:57:15 -0400

    feed the data via csv file

--- src/gatling/scala/MyGatlingRunner.scala
+++ src/gatling/scala/MyGatlingRunner.scala
@@ -1,5 +1,6 @@
 import io.gatling.app.Gatling
 import io.gatling.core.config.GatlingPropertiesBuilder
+import simulation.CsvFeeder
 import simulations.{AddPauseTime, CheckResponseBodyAndExtract, CheckResponseCode, CodeReuseWithObject}
 
 object MyGatlingRunner {
@@ -10,7 +11,8 @@ object MyGatlingRunner {
     //val simClass = classOf[AddPauseTime].getName
     //val simClass = classOf[CheckResponseCode].getName
     //val simClass = classOf[CheckResponseBodyAndExtract].getName
-    val simClass = classOf[CodeReuseWithObject].getName
+    //val simClass = classOf[CodeReuseWithObject].getName
+    val simClass = classOf[CsvFeeder].getName
 
     val props = new GatlingPropertiesBuilder
     props.simulationClass(simClass)
