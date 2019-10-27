90e4851771ccde2945c30e0f9af247782d92a11f
neil xavier dsouza <neil.xavier.dsouza@gmail.com>
2019-10-27 06:50:28 -0400

    check response code

--- src/gatling/scala/MyGatlingRunner.scala
+++ src/gatling/scala/MyGatlingRunner.scala
@@ -1,13 +1,14 @@
 import io.gatling.app.Gatling
 import io.gatling.core.config.GatlingPropertiesBuilder
-import simulations.AddPauseTime
+import simulations.{AddPauseTime, CheckResponseCode}
 
 object MyGatlingRunner {
 
   def main(args: Array[String]): Unit = {
 
     //val simClass = classOf[MyFirstTest].getName
-    val simClass = classOf[AddPauseTime].getName
+    //val simClass = classOf[AddPauseTime].getName
+    val simClass = classOf[CheckResponseCode].getName
 
     val props = new GatlingPropertiesBuilder
     props.simulationClass(simClass)
---
+++ src/gatling/scala/simulations/CheckResponseCode.scala
@@ -0,0 +1,23 @@
+package simulations
+
+import baseConfig.BaseSimulation
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class CheckResponseCode extends BaseSimulation {
+  val scn = scenario("Video Game DB")
+    .exec(http("Get all Video Games - 1st call")
+      .get("videogames")
+      .check(status.is(200)))
+
+    .exec(http("Get specific game")
+      .get("videogames/1")
+        .check(status.in(200 to 210)))
+
+    .exec(http("Get all Video games - 2nd call")
+        .get("videogames")
+        .check(status.not(404), status.not(500)))
+
+  setUp(scn.inject(atOnceUsers(1))).protocols(httpConf)
+}
