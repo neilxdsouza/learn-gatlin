a4faf2a41f111c74d7a644bafffe465c70b4ed1d
neil xavier dsouza <neil.xavier.dsouza@gmail.com>
2019-10-27 04:20:50 -0400

    common base class used in the test

--- src/gatling/scala/MyFirstTest.scala
+++ src/gatling/scala/MyFirstTest.scala
@@ -1,12 +1,16 @@
+import baseConfig.BaseSimulation
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-class MyFirstTest extends Simulation {
+class MyFirstTest extends BaseSimulation {
 
   // 1 Common HTTP Configuration
+  /*
   val httpConf = http
     .baseURL("http://localhost:8080/app/")
     .header("Accept", "application/json")
+
+   */
 
   // 2 Scenario Definition
   val scn = scenario("My first Test")
